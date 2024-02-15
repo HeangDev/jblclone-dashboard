7380ee6b1e178abf5fb6aaba54dec4464825d858
heangdeveloper <simkimheang4@gmail.com>
2024-02-15 10:03:03 +0700

    update file

--- src/pages/employee/Index.js
+++ src/pages/employee/Index.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2'
+import axios from 'axios'
 
 import $ from "jquery";
 import "jquery/dist/jquery.min.js";
 import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css'
 import 'datatables.net-bs5/js/dataTables.bootstrap5'
+import 'datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css'
+import 'datatables.net-responsive-bs5/js/responsive.bootstrap5'
 
 const List = () => {
     useEffect(() => {
@@ -17,7 +21,7 @@ const List = () => {
                         destroy: true,
                         responsive: true,
                     });
-                }, 500);
+                }, 0);
             });
         }
     }, [])
@@ -35,25 +39,35 @@ const List = () => {
                     </div>
                 </div>
                 <div className="card-body">
-                    <div className="table-responsive-sm">
-                        <table className="table table-striped" id="tbl_employee">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Name</th>
-                                    <th>Gender</th>
-                                    <th>Phone Number</th>
-                                    <th>Email</th>
-                                    <th>Hire Date</th>
-                                    <th>Status</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-
-                            </tbody>
-                        </table>
-                    </div>
+                    <table className="table table-striped" id="tbl_employee" style={{width: "100%"}}>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Gender</th>
+                                <th>Phone Number</th>
+                                <th>Email</th>
+                                <th>Hire Date</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Sim Kimheang</td>
+                                <td>Male</td>
+                                <td>011263262</td>
+                                <td>simkimheang4@gmail.com</td>
+                                <td>01/01/2021</td>
+                                <td><div className="badge text-bg-success">Working</div></td>
+                                <td>
+                                    <button type="button" className="btn btn-danger btn-sm me-2"><i className="bi bi-trash3"></i> Delete</button>
+                                    <button type="button" className="btn btn-info btn-sm"><i className="bi bi-pencil-square"></i> Edit</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </>
